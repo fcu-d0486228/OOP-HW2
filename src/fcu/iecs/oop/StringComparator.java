06f084ac3bb1020f636e0a9b8d4f298090bbda4d
D² MOZ <D MOZ>
2016-10-14 18:09:52 +0800

    一樣:結束 不一樣:重來

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -6,16 +6,20 @@ public class StringComparator {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		System.out.print("Enter a string 1: ");
-		String s1 = keyboard.next();
-		System.out.print("Enter a string 2: ");
-		String s2 = keyboard.next();
-		float a = s1.compareToIgnoreCase(s2);
-		if(a == 0){
-			System.out.println("The two strings are the same.");
-		}
-		else{
-			System.out.println("The two strings are not the same.");
+		while(true){
+			System.out.print("Enter a string 1: ");
+			String s1 = keyboard.next();
+			System.out.print("Enter a string 2: ");
+			String s2 = keyboard.next();
+			float a = s1.compareToIgnoreCase(s2);
+			if(a == 0){
+				System.out.println("The two strings are the same.");
+				break;
+			}
+			else{
+				System.out.println("The two strings are not the same.");
+				//continue;
+			}
 		}
 	}
 
